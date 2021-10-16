026da50c4423462e85aa9ca1048d71d935bfe016
Bretbret18 <bret.sperry@gmail.com>
2021-10-16 17:07:33 -0400

    UI and Add Book

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,48 @@
 // BOOK LIST PROJECT // ***
 
+// Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+function UI() {
+    UI.prototype.addBookToList = function(book){
+        console.log(book);
+      }
+}
+
+// class script {
+    
+// }
+
+
+// Add Book to List
+
+
+
+// Event Listeners
+document.querySelector('#book-form').addEventListener('submit', 
+function(e) {
+    // Get form values
+    const title = document.querySelector('#title').value,
+    author = document.querySelector('#author').value,
+    isbn = document.querySelector('#isbn').value;
+
+    // Instantiating book
+    const book = new Book(title, author, isbn);
+
+    // instantiate ui
+    const ui = new UI();
+    console.log(ui);
+
+    // Add book to list
+    ui.addBookToList(book);
+
+console.log(book);
+
+    
+    e.preventDefault();
+})
+
