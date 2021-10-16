a9b2776faecca31b88e3e405c6b8886d5cd313b6
Bretbret18 <bret.sperry@gmail.com>
2021-10-16 17:58:08 -0400

    display alerts

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,55 +6,81 @@ function Book(title, author, isbn) {
     this.isbn = isbn;
 }
 
-function UI() {}
+function UI() { }
 
 // Add book to list constructor function
-UI.prototype.addBookToList = function(book) {
-  const list = document.querySelector('#book-list');
-  // Create tr element
-  const row = document.createElement('tr');
-// Insert cols
-  row.innerHTML = `
+UI.prototype.addBookToList = function (book) {
+    const list = document.querySelector('#book-list');
+    // Create tr element
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
   <td>${book.title}</td>
   <td>${book.author}</td>
   <td>${book.isbn}</td>
   <td><a href="#" class="delete">X</a></td>
   `;
 
+    list.appendChild(row);
+}
+    // Show Alert
+    UI.prototype.showAlert = function(message, className) {
+        // Create Div
+        const div = document.createElement('div');
+        // Add Classes
+        div.className = `alert ${className}`;
+        // Add Text
+        div.appendChild(document.createTextNode(message));
+        // Get Parent
+        const container = document.querySelector('.container');
+        // Get Form
+        const form = document.querySelector('#book-form');
+        // Insert Alert
+        container.insertBefore(div, form);
 
-  list.appendChild(row)
+        // Timeout after 3 seconds
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
 
-  // CLear fields
-  UI.prototype.clearFields = function(){
+ // CLear fields
+ UI.prototype.clearFields = function () {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
     document.querySelector('#isbn').value = '';
-  }
 };
 
-
-
-
 // Event Listeners
-document.querySelector('#book-form').addEventListener('submit', 
-function(e) {
-    // Get form values
-    const title = document.querySelector('#title').value,
-    author = document.querySelector('#author').value,
-    isbn = document.querySelector('#isbn').value;
+document.querySelector('#book-form').addEventListener('submit',
+    function (e) {
+        // Get form values
+        const title = document.querySelector('#title').value,
+            author = document.querySelector('#author').value,
+            isbn = document.querySelector('#isbn').value;
 
-    // Instantiating book
-    const book = new Book(title, author, isbn);
-    console.log(book);
-    // instantiate ui
-    const ui = new UI();
-    console.log(ui);
-    // Add book to list
-    ui.addBookToList(book);
+        // Instantiating book
+        const book = new Book(title, author, isbn);
+        console.log(book);
+        // instantiate ui
+        const ui = new UI();
+        console.log(ui);
 
-    // Clear fields
-    ui.clearFields()
-    
-    e.preventDefault();
-})
+        // Validate
+        if(title === '' || author === '' || isbn === '') {
+          // Error Alert
+        ui.showAlert('Please fill in all fields', 'error');
+        } else {
+            // Add book to list
+        ui.addBookToList(book);
+
+        // Show Success
+        ui.showAlert('Book Added!', 'success');
+
+        // Clear fields
+        ui.clearFields()
+        }
+
+        e.preventDefault();
+    });
 
